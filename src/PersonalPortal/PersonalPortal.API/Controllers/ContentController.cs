5f5bed63d747c32ec1ded2e7dee6aa44b58fc313
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-08 02:16:19 -0700

    Added method GET to ContentController

--- src/PersonalPortal/PersonalPortal.API/Controllers/ContentController.cs
+++ src/PersonalPortal/PersonalPortal.API/Controllers/ContentController.cs
@@ -5,6 +5,11 @@ namespace PersonalPortal.API.Controllers
 {
     public class ContentController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="medium"></param>
+        /// <returns></returns>
         [HttpPut]
         [Route("Medium/Create")]
         public IActionResult RegisterContentMedium([FromBody] ContentMedium medium)
@@ -19,6 +24,24 @@ namespace PersonalPortal.API.Controllers
         public IActionResult GetAllMediums()
         { 
             var result = new List<ContentMedium>();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("Medium/Get/{name}")]
+        public IActionResult GetMedium(string filter)
+        { 
+            var result = new ContentMedium();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Medium/Update/{id}")]
+        public IActionResult UpdateMedium([FromBody] ContentMedium medium)
+        {
+            var result = medium;
 
             return Ok(result);
         }
