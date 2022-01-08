b4e1a368dacdd372fa74b05bf7b54bb4d8a8baf2
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-08 12:26:24 -0700

    Created Service and Contract for ContentController

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IContentService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IContentService
+    {
+        Task<IActionResult> RegisterContentMedium(ContentMedium medium);
+        Task<IActionResult> GetAllMediums();
+        Task<IActionResult> GetMedium(string filter);
+        Task<IActionResult> UpdateMedium(ContentMedium medium);
+        Task<IActionResult> DeleteMedium(int id);
+        Task<IActionResult> ClearMediums();
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/ContentService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class ContentService : IContentService
+    {
+    }
+}
