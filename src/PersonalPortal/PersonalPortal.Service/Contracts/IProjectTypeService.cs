436710d2a09b52a496800d9494901fc25a3bfc99
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-11 13:35:53 -0700

    Added ProjectTypeService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IProjectTypeService.cs
@@ -0,0 +1,14 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IProjectTypeService
+    {
+        Task<bool> RegisterProjectType(ProjectType projectType);
+        Task<List<ProjectType>> GetAllProjectTypes();
+        Task<List<ProjectType>> GetProjectTypeByName(string filter);
+        Task<ProjectType> GetProjectTypeById(int id);
+        Task<bool> UpdateProjectType(ProjectType projectType);
+        Task<bool> DeleteProjectType(int id);
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/ProjectTypeService.cs
@@ -0,0 +1,38 @@
+﻿using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class ProjectTypeService : IProjectTypeService
+    {
+        public Task<bool> DeleteProjectType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProjectType>> GetAllProjectTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectType> GetProjectTypeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProjectType>> GetProjectTypeByName(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterProjectType(ProjectType projectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProjectType(ProjectType projectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
