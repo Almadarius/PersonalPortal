1c82229415a93bc961d0776aa02472260cc06275
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-10 21:16:28 -0700

    Added PlatformService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IPlatformService.cs
@@ -0,0 +1,14 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IPlatformService
+    {
+        Task<bool> RegisterPlatform(Platform platform);
+        Task<List<Platform>> GetAllPlatforms();
+        Task<List<Platform>> GetSPlatformByName(string filter);
+        Task<Platform> GetPlatformById(int id);
+        Task<bool> UpdatePlatform(StaticResource staticResource);
+        Task<bool> DeletePlatform(int id);
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/PlatformService.cs
@@ -0,0 +1,38 @@
+﻿using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class PlatformService : IPlatformService
+    {
+        public Task<bool> DeletePlatform(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Platform>> GetAllPlatforms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Platform> GetPlatformById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Platform>> GetSPlatformByName(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterPlatform(Platform platform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePlatform(StaticResource staticResource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
