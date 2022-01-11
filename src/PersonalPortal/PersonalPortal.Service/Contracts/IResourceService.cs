3716eaa57835528401f7f638fd8c1bb3e04a26f0
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-10 21:21:34 -0700

    Added ResourceService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IResourceService.cs
@@ -0,0 +1,14 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IResourceService
+    {
+        Task<bool> RegisterResource(Resource staticResource);
+        Task<List<Resource>> GetAllResources();
+        Task<List<Resource>> GetResourceByName(string filter);
+        Task<Resource> GetResourceById(int id);
+        Task<bool> UpdateResource(Resource staticResource);
+        Task<bool> DeleteResource(int id);
+    }
+}
