c7402311fc51c660d87f399e5a18389a4aa6d632
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-10 22:34:29 -0700

    Added ResourceCategoryTypeService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IResourceCategoryTypeService.cs
@@ -0,0 +1,13 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IResourceCategoryTypeService
+    {
+        Task<bool> RegisterResourceCategoryType(int resourceCategoryId, int resourceTypeId);
+        Task<List<ResourceCategory>> GetAllResourceCategoriesByType(int resourceTypeId);
+        Task<List<ResourceCategory>> GetResourceCategoryByName(string filter);
+        Task<ResourceCategory> GetResourceCategoryById(int id);        
+        Task<bool> DeleteResourceCategoryType(int resourceCategoryId, int resourceTypeId);
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/ResourceCategoryTypeService.cs
@@ -0,0 +1,33 @@
+﻿using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class ResourceCategoryTypeService : IResourceCategoryTypeService
+    {
+        public Task<bool> DeleteResourceCategoryType(int resourceCategoryId, int resourceTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResourceCategory>> GetAllResourceCategoriesByType(int resourceTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResourceCategory> GetResourceCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResourceCategory>> GetResourceCategoryByName(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterResourceCategoryType(int resourceCategoryId, int resourceTypeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
