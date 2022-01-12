654d1947ded648a7c283c5285f79a6b17424605f
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-11 17:13:12 -0700

    Added ResumeEducationInfoService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IResumeEducationInfoService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IResumeEducationInfoService
+    {
+        Task<bool> RegisterResumeEducationInfo(int resumeId, int educationInfo);
+        Task<List<EducationInfo>> GetAllEducationInfoRecordsByResume(int resumeId);
+        Task<bool> DeleteResumeEducationInfo(int resumeId, int educationInfo);
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/ResumeEducationInfoService.cs
@@ -0,0 +1,23 @@
+﻿using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class ResumeEducationInfoService : IResumeEducationInfoService
+    {
+        public Task<bool> DeleteResumeEducationInfo(int resumeId, int educationInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<EducationInfo>> GetAllEducationInfoRecordsByResume(int resumeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterResumeEducationInfo(int resumeId, int educationInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
