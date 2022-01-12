f1d2a726b426ec68a5e1a8d6ff759e77eee7d533
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-11 17:16:39 -0700

    Added ResumeContactInfoService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IResumeContactInfoService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IResumeContactInfoService
+    {
+        Task<bool> RegisterResumeContactInfo(int resumeId, int contactInfoId);
+        Task<List<ContactInfo>> GetAllContactInfoRecordsByResume(int resumeId);
+        Task<bool> DeleteContactInfo(int resumeId);
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/ResumeContactInfoService.cs
@@ -0,0 +1,23 @@
+﻿using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class ResumeContactInfoService : IResumeContactInfoService
+    {
+        public Task<bool> DeleteContactInfo(int resumeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ContactInfo>> GetAllContactInfoRecordsByResume(int resumeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterResumeContactInfo(int resumeId, int contactInfoId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
