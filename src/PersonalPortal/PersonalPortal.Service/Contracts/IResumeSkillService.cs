22e661eb09e70e4e4187e43840dbadafc3746ec2
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-11 17:20:58 -0700

    Added ResumeSkillService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IResumeSkillService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IResumeSkillService
+    {
+        Task<bool> RegisterResumeSkill(int resumeId, int skillId);
+        Task<List<Skill>> GetAllSkillsByResume(int resumeId);        
+        Task<bool> DeleteResumeSkill(int resumeId);
+    }
+}
