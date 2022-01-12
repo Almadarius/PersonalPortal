2e4f0c7b1e56719a7119fe9b52b0cddaaff8871e
Almadar <ing.emmanuelmedina@gmail.com>
2022-01-11 17:09:13 -0700

    Added ResumeInterestService

---
+++ src/PersonalPortal/PersonalPortal.Service/Contracts/IResumeInterestService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalPortal.Models.Models;
+
+namespace PersonalPortal.Service.Contracts
+{
+    public interface IResumeInterestService
+    {
+        Task<bool> RegisterResumeInterest(int resumeId, int interestId);
+        Task<List<Interest>> GetAllInterestsByResume(int resumeId);        
+        Task<bool> DeleteResumeInterest(int resumeId);
+    }
+}
---
+++ src/PersonalPortal/PersonalPortal.Service/Services/ResumeInterestService.cs
@@ -0,0 +1,23 @@
+﻿using PersonalPortal.Models.Models;
+using PersonalPortal.Service.Contracts;
+
+namespace PersonalPortal.Service.Services
+{
+    public class ResumeInterestService : IResumeInterestService
+    {
+        public Task<bool> DeleteResumeInterest(int resumeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Interest>> GetAllInterestsByResume(int resumeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterResumeInterest(int resumeId, int interestId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
